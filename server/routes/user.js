51e4f04b850263b928ad4636099593fa76203dcf
TolyaIvanov <ivanov.anatoliu@list.ru>
2019-07-07 13:13:21 +0300

    done without backend

--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const UserController = require('./../controllers/UserController');
-
-router.get('/vklogin', UserController.vklogin);
-
-module.exports = router;
